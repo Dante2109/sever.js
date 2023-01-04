ea907b236261626d0ce1604941efbb4f662430bf
Dante2109 <akshayverma4299@gmail.com>
2023-01-05 00:31:27 +0530

    server

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 const PORT= process.env.PORT || 8080
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
